8e49306414c10566f7d9a0dd6b54374aa5f86b04
LisandroNh3 <00025825@uca.edu.sv>
2025-04-05 10:09:03 -0600

    Creacion de carpetas

---
+++ model/Artista.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Artista {
+    private int id;
+    private String nombre;
+    private String discoMasVendido;
+    private int ventasTotales;
+}
---
+++ model/Disco.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Disco {
+    private Artista artista;
+    private String titulo;
+    private int ventasTotales;
+}
