586998cafce8691f2a69411472372b441234e5bc
danielmr28 <00085920@uca.edu.sv>
2025-04-05 10:31:31 -0600

    Artista model

--- model/Artista.java
+++ model/Artista.java
@@ -5,4 +5,29 @@ public class Artista {
     private String nombre;
     private String discoMasVendido;
     private int ventasTotales;
+
+    public Artista(int Id, String nombre){
+        this.id =id;
+        this.nombre = nombre;
+        this.discoMasVendido = "";
+        this.ventasTotales = 0;
+    }
+
+    public int getId() {return id;}
+    public String getNombre(){return nombre;}
+    public String getdiscoMasVendivo(){return discoMasVendido;}
+    public int getventasTotales(){return ventasTotales;}
+
+    public void setNombre(String nombre){this.nombre = nombre;}
+    public void setDiscoMasVendido(String discoMasVendido){this.discoMasVendido = discoMasVendido;}
+    public void setVentasTotales(int ventasTotales){this.ventasTotales = ventasTotales;}
+
+    public void agregarVentas(int cantidad){
+        this.ventasTotales += cantidad;
+    }
+
+    @Override
+    public String toString(){
+        return "Artista ID" + id + ", Nombre" + nombre +  ", Disco más vendido" + discoMasVendido + ", Ventas totales" + ventasTotales;
+    }
 }
